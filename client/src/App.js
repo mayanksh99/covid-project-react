75327a3efdadb51f88ec1f55b09f4333d5d5c60c
Mayank Shakya <mayank.1822it1081@kiet.edu>
2020-07-19 00:30:46 +0530

    --update: add login page

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Route, Switch, Redirect } from "react-router-dom";
+import "./App.css";
+import Login from "./components/Authentication/Login";
+import "antd/dist/antd.css";
+import DoctorNavigator from "./components/Layout/DoctorNavigator";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Switch>
+      <Route exact path="/login" component={Login} />
+      <Route exact path="/doctor" component={DoctorNavigator} />
+      <Redirect from="/" to="/login" />
+    </Switch>
   );
-}
+};
 
 export default App;

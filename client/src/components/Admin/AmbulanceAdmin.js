447efadabf42abbe56258f75ae48d32f6af3cc89
Mayank Shakya <mayank.1822it1081@kiet.edu>
2020-07-22 22:54:18 +0530

    :zap: --update: ambulance for master admin complete

--- client/src/components/Admin/AmbulanceAdmin.js
+++ client/src/components/Admin/AmbulanceAdmin.js
@@ -1,14 +1,16 @@
-import React, { useState } from "react";
+import React from "react";
 import "./style.css";
-import { Row, Statistic, Col, Button, Table, Input } from "antd";
-import AddAmbulanceAdmin from "./AddAmbulanceAdmin";
+import { Col, Table, Input, Card, Popconfirm, Divider, Tooltip } from "antd";
+import AmbulanceAdminOption from "./AmbulanceAdminOption";
+import {
+	DeleteOutlined,
+	EditOutlined,
+	CloseCircleOutlined,
+	CheckCircleOutlined
+} from "@ant-design/icons";
+import { Link } from "react-router-dom";
 
 const AmbulanceAdmin = () => {
-	const [showModal, setshowModal] = useState(false);
-
-	const handelModal = value => {
-		setshowModal(value);
-	};
 	const columns = [
 		{
 			title: "#",
@@ -18,7 +20,8 @@ const AmbulanceAdmin = () => {
 		{
 			title: "Name",
 			dataIndex: "name",
-			key: "name"
+			key: "name",
+			render: name => <Link to="/ambulancedetails/sdvsdvsd">{name}</Link>
 		},
 		{
 			title: "Phone",
@@ -39,10 +42,47 @@ const AmbulanceAdmin = () => {
 			title: "Action",
 			dataIndex: "action",
 			key: "action",
-			render: () => (
-				<Button type="primary" className="login-form-button">
-					Details
-				</Button>
+			render: action => (
+				<>
+					<Popconfirm
+						title="Do you want to toggle user block?"
+						// onConfirm={() => handleUserRevoke(action[1])}
+						okText="Yes"
+						cancelText="No"
+					>
+						{action ? (
+							<Tooltip title="Unblock user">
+								<CloseCircleOutlined
+									style={{ color: "#DB4437" }}
+								/>
+							</Tooltip>
+						) : (
+							<Tooltip title="Block user">
+								<CheckCircleOutlined
+									style={{ color: "#0F9D58" }}
+								/>
+							</Tooltip>
+						)}
+					</Popconfirm>
+					<Divider type="vertical" />
+					<Tooltip title="Edit password">
+						<Link to="#">
+							<EditOutlined style={{ color: "#F4B400" }} />
+						</Link>
+					</Tooltip>
+
+					<Divider type="vertical" />
+					<Tooltip title="Delete admin">
+						<Popconfirm
+							title="Are you sure delete this user?"
+							// onConfirm={() => handleUserDelete(action[1])}
+							okText="Yes"
+							cancelText="No"
+						>
+							<DeleteOutlined style={{ color: "#DB4437" }} />
+						</Popconfirm>
+					</Tooltip>
+				</>
 			)
 		}
 	];
@@ -51,80 +91,70 @@ const AmbulanceAdmin = () => {
 		{
 			key: "1",
 			name: "John Brown",
-			phone: +915864268542,
+			phone: "+915864268542",
 			areapin: 201206,
 			count: 56,
-			action: "Detail"
+			action: true
 		},
 		{
 			key: "2",
 			name: "John Black",
-			phone: +915864268542,
+			phone: "+915864268542",
 			areapin: 201206,
 			count: 56,
-			action: "Detail"
+			action: false
 		},
 		{
 			key: "3",
 			name: "John Blue",
-			phone: +915864268542,
+			phone: "+915864268542",
 			areapin: 201206,
 			count: 56,
-			action: "Detail"
+			action: true
 		}
 	];
 
 	return (
-		<div>
+		<>
 			<h2 className="login-card-head">Ambulance</h2>
-			<br />
-			<Row>
-				<Col xl={8} lg={8} md={12} sm={24} xs={24}>
-					<Statistic
-						title="Total"
-						value={112893}
-						valueStyle={{ color: "#005ea5", fontWeight: 600 }}
-					/>
-				</Col>
-				<Col xl={8} lg={8} md={12} sm={24} xs={24}>
-					<Statistic
-						title="Available"
-						value={112893}
-						valueStyle={{ color: "#005ea5", fontWeight: 600 }}
-					/>
-				</Col>
-				<Col xl={8} lg={8} md={12} sm={24} xs={24}>
-					<Button
-						type="primary"
-						className="login-form-button"
-						onClick={() => handelModal(true)}
-					>
-						Add Administrator
-					</Button>
-				</Col>
-			</Row>
-			<br />
-			<div>
+			<AmbulanceAdminOption />
+			<div className="table-wrapper-card">
 				<h3 style={{ fontSize: "16px" }}>
 					List of Ambulance Administrator
 				</h3>
-				<Col span={6}>
-					<Input.Search
-						type="text"
-						style={{ width: 200, marginBottom: 12 }}
-						placeholder="Search"
-						allowClear
-					/>
-				</Col>
-
-				<Table
-					columns={columns}
-					dataSource={data}
-					pagination={{ position: ["bottomCenter"] }}
-				/>
+				<div>
+					<Col span={6}>
+						<Input.Search
+							type="text"
+							style={{ width: 200, marginBottom: 12 }}
+							placeholder="Search"
+							allowClear
+						/>
+					</Col>
+					<Card
+						style={{ padding: 0, width: "100%", overflowX: "auto" }}
+					>
+						<Table
+							columns={columns}
+							dataSource={data}
+							pagination={{ position: ["bottomCenter"] }}
+						/>
+					</Card>
+				</div>
 			</div>
-			<AddAmbulanceAdmin visible={showModal} handleModal={handelModal} />
-		</div>
+			{/* <Drawer
+				title="Update Profile"
+				placement="right"
+				closable={true}
+				destroyOnClose={true}
+				onClose={() => setProfileDrawer(false)}
+				visible={profileDrawer}
+			>
+				<UpdateProfile 
+				user={user} onUpdateUser={handleUpdateUser} 
+				/>
+			</Drawer> */}
+		</>
 	);
 };
 

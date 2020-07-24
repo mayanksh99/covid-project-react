524470e2a9f381ca849671b207adb9a718dcf88f
ItsNidhaan <nidhaan.1923ec1105@kiet.edu>
2020-07-24 12:06:36 +0530

    nothing much

--- client/src/components/Hospital/AssignBed.js
+++ client/src/components/Hospital/AssignBed.js
@@ -3,6 +3,8 @@ import "antd/dist/antd.css";
 import { Table } from "antd";
 import { Input } from "antd";
 import { AudioOutlined } from "@ant-design/icons";
+import { Button } from 'antd';
+import { Row, Col } from 'antd';
 
 const AssignBed = () => {
 	const { Search } = Input;
@@ -36,17 +38,7 @@ const AssignBed = () => {
 			dataIndex: "age",
 			key: "age"
 		},
-		{
-			title: "Query",
-			key: "Query"
-			// render: ()=>{
-			// 	<Search
-			// 		placeholder="input search text"
-			// 		onSearch={value => console.log(value)}
-			// 		style={{ width: 200 }}
-			//     />
-			// }
-		}
+		
 	];
 
 	const data = [];
@@ -57,14 +49,49 @@ const AssignBed = () => {
 			name: `Edward King ${i}`,
 			severity: "L2",
 			gender: `Male`,
-			age: "43 Years"
+			age: "43 Years",
+			// render: ()=>{
+			// 	<Button type="primary">Assign Beds</Button>
+			// }
+			
 		});
 	}
 
 	return (
 		<div>
 			<h1>Assign Beds</h1>
-
+			<Row>
+				<Col span={12}>Total Patients</Col>
+				
+				
+				<Col span={12}>
+				<Search
+							placeholder="Search Patients"
+							onSearch={value => console.log(value)}
+							style={{ width: 200 }}
+					/>
+				</Col>
+			</Row>
+			<Row>
+				<Col span={12}>28</Col>
+				<Col span={12}></Col>
+			</Row>
+			
+			<Row>
+				<Col span={8}>Total beds</Col>
+				<Col span={8}>Avaliable Beds</Col>
+				<Col span={8}>
+					<Button type="primary">Assign Beds</Button>
+				</Col>
+			</Row>
+			<Row>
+				<Col span={8}>100</Col>
+				<Col span={8}>45</Col>
+				<Col span={8}>
+					
+				</Col>
+			</Row>
+			
 			<Table
 				size="middle"
 				title={() => "list of patients to assign beds"}

a0d6735e0ed4e1b91a42b9f91b4698788ef4f42b
ritiksr25 <ritiksr25@gmail.com>
2020-08-09 20:36:07 +0530

    --update: api url

--- src/utils/services.js
+++ src/utils/services.js
@@ -40,8 +40,8 @@ import {
 	ADD_AMBULANCE
 } from "./routes";
 
-const BASE_URL = "https://c2614deb9f12.ngrok.io/api/v1";
-export const EndPoint = "https://c2614deb9f12.ngrok.io";
+const BASE_URL = "https://covid-project-gzb.herokuapp.com//api/v1";
+export const EndPoint = "https://covid-project-gzb.herokuapp.com/";
 
 axios.defaults.baseURL = BASE_URL;
 

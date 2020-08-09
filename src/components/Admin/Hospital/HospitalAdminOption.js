7b91d5b9ed876dde9a2c4320b0ba7595c1af24e0
Aniket Padmansh <60324594+padmansh@users.noreply.github.com>
2020-08-09 14:48:00 +0530

    bulk add hospitals added

--- src/components/Admin/Hospital/HospitalAdminOption.js
+++ src/components/Admin/Hospital/HospitalAdminOption.js
@@ -1,14 +1,87 @@
 import React, { useState } from "react";
-import { Row, Col, Button, Skeleton } from "antd";
+import { Row, Col, Button, Skeleton, Upload } from "antd";
+import { UploadOutlined } from "@ant-design/icons";
 import AddHospital from "./AddHospital";
 import PageStats from "../../common/PageStats";
+import { _notification } from "../../../utils/_helper";
+import AddBulkResponseModal from "../../../utils/_helper";
 
 const HospitalAdminOption = ({ refresh, setRefresh, stats, isLoading }) => {
+	let AUTH_TOKEN = JSON.parse(localStorage.getItem("token"));
+	const [bulkUploadDetails, setBulkUploadDetails] = useState(false);
+	const [isResultsVisible, setIsResultsVisible] = useState(false);
+	const [data, setData] = useState(null);
 	const [showModal, setShowModal] = useState(false);
 
 	const handleModal = value => {
 		setShowModal(value);
 	};
+
+	const closeResults = () => {
+		setIsResultsVisible(false);
+	};
+
+	let i = 1;
+	const props = {
+		name: "file",
+		action: `https://covid-project-gzb.herokuapp.com/api/v1/hospitals/bulk`,
+		headers: {
+			"x-auth-token": `${AUTH_TOKEN.token}`
+		},
+		onChange(info) {
+			if (info.file.status === "done") {
+				console.log(info.file.response);
+				if (info.file.response.data.invalidHospitals.length === 0) {
+					_notification(
+						"success",
+						"Success",
+						"All hospitals were added successfully !"
+					);
+				} else {
+					setData(
+						info.file.response.data.invalidHospitals.map(h => {
+							return {
+								key: i++,
+								hospital: h.hospital,
+								reason: h.error
+							};
+						})
+					);
+					setBulkUploadDetails(info.file.response.data);
+					_notification(
+						"error",
+						"Attention !",
+						"Hospital addition failed. Please Check !"
+					);
+					setIsResultsVisible(true);
+				}
+			} else if (info.file.status === "error") {
+				_notification(
+					"error",
+					"Error",
+					"Upload failed. Please try again later !"
+				);
+			}
+		}
+	};
+
+	const tableColumns = [
+		{
+			title: "#",
+			dataIndex: "key",
+			key: "key"
+		},
+		{
+			title: "Hospital Name",
+			dataIndex: "hospital",
+			key: "hospital"
+		},
+		{
+			title: "Reason",
+			dataIndex: "reason",
+			key: "reason"
+		}
+	];
 
 	return (
 		<>
@@ -133,15 +206,47 @@ const HospitalAdminOption = ({ refresh, setRefresh, stats, isLoading }) => {
 						</Col>
 					) : null}
 					<Col xl={4} lg={4} md={4} sm={24} xs={24}>
-						<div>
-							<Button
-								type="primary"
-								className="login-form-button"
-								onClick={handleModal}
-							>
-								Add Hospital
-							</Button>
-						</div>
+						<Row gutter={[16, 16]}>
+							<Col xl={24} lg={24} md={24} sm={24} xs={12}>
+								<Row gutter={[16, 16]}>
+									<Col
+										xl={24}
+										lg={24}
+										md={24}
+										sm={24}
+										xs={12}
+									>
+										<Button
+											type="primary"
+											className="login-form-button"
+											onClick={handleModal}
+										>
+											Add Hospital
+										</Button>
+									</Col>
+								</Row>
+							</Col>
+						</Row>
+						<Row gutter={[16, 16]}>
+							<Col xl={24} lg={24} md={24} sm={24} xs={12}>
+								<Row gutter={[16, 16]}>
+									<Col
+										xl={24}
+										lg={24}
+										md={24}
+										sm={24}
+										xs={12}
+									>
+										<Upload accept=".csv" {...props}>
+											<Button>
+												<UploadOutlined />
+												Upload CSV
+											</Button>
+										</Upload>
+									</Col>
+								</Row>
+							</Col>
+						</Row>
 					</Col>
 				</Row>
 			</Skeleton>
@@ -150,6 +255,15 @@ const HospitalAdminOption = ({ refresh, setRefresh, stats, isLoading }) => {
 				handleModal={handleModal}
 				refresh={refresh}
 				setRefresh={setRefresh}
+			/>
+			<AddBulkResponseModal
+				isResultsVisible={isResultsVisible}
+				closeResults={closeResults}
+				tableColumns={tableColumns}
+				data={data}
+				bulkUploadDetails={bulkUploadDetails}
+				title={"Invalid Hospitals"}
+				whatIsBeingAdded={"Hospital"}
 			/>
 		</>
 	);

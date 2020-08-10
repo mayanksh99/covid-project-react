8c7f8be96efc6e4edc02739e20bfac232388ea18
Mayank Shakya <mayank.1822it1081@kiet.edu>
2020-08-10 20:14:21 +0530

    :zap: update: search patient module added

--- src/components/Admin/Patients.js
+++ src/components/Admin/Patients.js
@@ -1,11 +1,14 @@
 import React, { useState, useEffect } from "react";
-import { Table, Card, Tag } from "antd";
+import { Table, Card, Tag, Row, Col, Input, Select } from "antd";
 import { getPatientsService } from "../../utils/services";
 import { _notification } from "./../../utils/_helper";
+
+const { Option } = Select;
 
 const Patients = () => {
 	const [isLoading, setIsLoading] = useState(false);
 	const [patients, setPatients] = useState(null);
+	const [search, setSearch] = useState(null);
 
 	useEffect(() => {
 		(async () => {
@@ -20,6 +23,36 @@ const Patients = () => {
 			}
 		})();
 	}, []);
+
+	const handleQuery = async val => {
+		setIsLoading(true);
+		setSearch(val);
+		try {
+			let params = {
+				search: val
+			};
+			const res = await getPatientsService(params);
+			setPatients(res.data);
+			setIsLoading(false);
+		} catch (err) {
+			_notification("warning", "Error", err.message);
+			setIsLoading(false);
+		}
+	};
+
+	// const handlePermission = async val => {
+	// 	setIsLoading(true);
+	// 	setPermission(val);
+	// 	try {
+	// 		let params = { permission: val, search };
+	// 		const res = await searchAdminsService(params);
+	// 		setAdmins(res.data);
+	// 		setIsLoading(false);
+	// 	} catch (err) {
+	// 		_notification("warning", "Error", err.message);
+	// 		setIsLoading(false);
+	// 	}
+	// };
 
 	const columns = [
 		{
@@ -95,6 +128,33 @@ const Patients = () => {
 
 	return (
 		<>
+			<Row>
+				<Col span={12}>
+					<Input.Search
+						className="input-field"
+						type="text"
+						style={{ width: 200, marginBottom: 12 }}
+						placeholder="Search"
+						allowClear
+						onSearch={value => handleQuery(value)}
+					/>
+				</Col>
+				{/* <Col span={12} style={{ float: "right" }}>
+					<div style={{ float: "right" }}>
+						<Select
+							placeholder="Select Permissions"
+							// onChange={handlePermission}
+							allowClear
+							className="input-field"
+						>
+							<Option value="master">Master</Option>
+							<Option value="doctor">Doctor</Option>
+							<Option value="hospital">Hospital</Option>
+							<Option value="ambulance">Ambulance</Option>
+						</Select>
+					</div>
+				</Col> */}
+			</Row>
 			<Card>
 				<p
 					style={{
